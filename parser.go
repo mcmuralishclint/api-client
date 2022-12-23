68ea3ccac70378ce0c779ed2ba46222d24b6c379
muralish.clinton <muralish.clinton@circles.asia>
2022-12-23 09:21:05 +0530

    parser

---
+++ parser.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	Info     interface{} `yaml:"info"`
+	Services map[string]Service
+}
+
+type Service struct {
+	Name    string `yaml:"name"`
+	BaseUrl string `yaml:"base_url"`
+	Version map[string]map[string]Endpoint
+}
+
+type Endpoint struct {
+	Path string
+	Verb string
+}
+
+func Parser(filename string) *Config {
+	ymlData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	configs := Config{}
+	yaml.Unmarshal(ymlData, &configs)
+	return &configs
+}
